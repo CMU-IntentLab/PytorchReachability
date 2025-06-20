f12d47f173d9ad0262b4dbd645941d6a0bef4f94
kensukenk <72209286+kensukenk@users.noreply.github.com>
2025-06-20 05:44:39 -0400

    Update load_models.py

--- helper/load_models.py
+++ helper/load_models.py
@@ -1,24 +1,9 @@
-import argparse
-import os
-import sys
-import pprint
-
 import gymnasium #as gym
 import gym
 import numpy as np
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(parent_dir)
-dreamer_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../dreamerv3-torch'))
-sys.path.append(dreamer_dir)
-saferl_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '/PyHJ'))
-sys.path.append(saferl_dir)
-print(sys.path)
-import models
-import tools
-import ruamel.yaml as yaml
 import wandb
 from PyHJ.data import Collector, VectorReplayBuffer
 from PyHJ.env import DummyVectorEnv
@@ -41,81 +26,61 @@ import matplotlib.pyplot as plt
 # note: need to include the dreamerv3 repo for this
 from dreamer import make_dataset
 from scripts.generate_data_traj_cont import get_frame
-from helper.load_config import load_config
 
 
 def load_models():
-    config = load_config("/content/PytorchReachability/configs.yaml")
-    env = gymnasium.make(config.task, params = [config])
-    config.num_actions = env.action_space.n if hasattr(env.action_space, "n") else env.action_space.shape[0]
-    wm = models.WorldModel(env.observation_space_full, env.action_space, 0, config)
+    env = gymnasium.make("dubins-wm-colab")
+    num_actions =  env.action_space.shape[0]
+    wm = models.WorldModel(env.observation_space_full, env.action_space, 0)
 
-    env = gymnasium.make(config.task, params = [config])
+    env = gymnasium.make("dubins-wm-colab")
 
     # check if the environment has control and disturbance actions:
     assert hasattr(env, 'action_space') #and hasattr(env, 'action2_space'), "The environment does not have control and disturbance actions!"
-    config.state_shape = env.observation_space.shape or env.observation_space.n
-    config.action_shape = env.action_space.shape or env.action_space.n
-    config.max_action = env.action_space.high[0]
+    state_shape = env.observation_space.shape or env.observation_space.n
+    action_shape = env.action_space.shape or env.action_space.n
+    max_action = env.action_space.high[0]
 
-    if config.actor_activation == 'ReLU':
-        actor_activation = torch.nn.ReLU
-    elif config.actor_activation == 'Tanh':
-        actor_activation = torch.nn.Tanh
-    elif config.actor_activation == 'Sigmoid':
-        actor_activation = torch.nn.Sigmoid
-    elif config.actor_activation == 'SiLU':
-        actor_activation = torch.nn.SiLU
+    actor_activation = torch.nn.ReLU
+    critic_activation = torch.nn.ReLU
+    
 
-    if config.critic_activation == 'ReLU':
-        critic_activation = torch.nn.ReLU
-    elif config.critic_activation == 'Tanh':
-        critic_activation = torch.nn.Tanh
-    elif config.critic_activation == 'Sigmoid':
-        critic_activation = torch.nn.Sigmoid
-    elif config.critic_activation == 'SiLU':
-        critic_activation = torch.nn.SiLU
-
-    if config.critic_net is not None:
-        critic_net = Net(
-            config.state_shape,
-            config.action_shape,
-            hidden_sizes=config.critic_net,
-            activation=critic_activation,
-            concat=True,
-            device=config.device
-        )
-    else:
-        # report error:
-        raise ValueError("Please provide critic_net!")
-
-    critic = Critic(critic_net, device=config.device).to(config.device)
-    critic_optim = torch.optim.AdamW(critic.parameters(), lr=config.critic_lr, weight_decay=config.weight_decay_pyhj)
+    critic_net = Net(
+        state_shape,
+        action_shape,
+        hidden_sizes=[128,128,128],
+        activation=critic_activation,
+        concat=True,
+        device="cuda"
+    )
+    
+    critic = Critic(critic_net, device='cuda').to('cuda')
+    critic_optim = torch.optim.AdamW(critic.parameters(), lr=0.001, weight_decay=0.001)
 
 
     from PyHJ.policy import avoid_DDPGPolicy_annealing as DDPGPolicy
 
     print("DDPG under the Avoid annealed Bellman equation with no Disturbance has been loaded!")
 
-    actor_net = Net(config.state_shape, hidden_sizes=config.control_net, activation=actor_activation, device=config.device)
+    actor_net = Net(state_shape, hidden_sizes=[128,128,128], activation=actor_activation, device="cuda")
     actor = Actor(
-        actor_net, config.action_shape, max_action=config.max_action, device=config.device
-    ).to(config.device)
-    actor_optim = torch.optim.AdamW(actor.parameters(), lr=config.actor_lr)
+        actor_net, action_shape, max_action=max_action, device="cuda"
+    ).to("cuda")
+    actor_optim = torch.optim.AdamW(actor.parameters(), lr=1e-4)
 
 
     policy = DDPGPolicy(
     critic,
     critic_optim,
-    tau=config.tau,
-    gamma=config.gamma_pyhj,
-    exploration_noise=GaussianNoise(sigma=config.exploration_noise),
-    reward_normalization=config.rew_norm,
-    estimation_step=config.n_step,
+    tau=0.005,
+    gamma=0.9999,
+    exploration_noise=GaussianNoise(sigma=0.1),
+    reward_normalization=False,
+    estimation_step=1,
     action_space=env.action_space,
     actor=actor,
     actor_optim=actor_optim,
-    actor_gradient_steps=config.actor_gradient_steps,
+    actor_gradient_steps=1,
     )
 
 
